fe827dc2210b62ae26e33f3bfd3acb0431925c6d
Daniel Sun <danielsun12345@gmail.com>
2020-04-02 00:47:26 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(hash = {title: "Home Alone", release_date: 1990})
   
     Movie.create do |m|
     m.title = hash[:title] 
-    m.release_date = hash[]
+    m.release_date = hash[:release_date]
     end
 end
 
